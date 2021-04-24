5bf0ff61d80779833fcb631d2ed5cb7e75f7648f
jackpunt <jpeckj@gmail.com>
2021-04-24 11:09:07 -0700

    ignore dist/

--- dist/out-tsc/echoserver.js
+++
@@ -1,9 +0,0 @@
-import { CnxManager, EchoServer } from "wspbserver";
-const theGraid = {
-    domain: ".thegraid.com",
-    port: 8443,
-    keydir: "/Users/jpeck/keys/"
-};
-console.log("echoserver! ", new Date().toTimeString());
-new CnxManager("game7", theGraid, EchoServer).run();
-//# sourceMappingURL=echoserver.js.map
