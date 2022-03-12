2e57f833a51fc0300ff5341cac16f7a3ee007e10
jackpunt <jpeckj@gmail.com>
2022-03-12 14:25:35 -0800

    reminder to build a gamma gameserver

--- src/gameserver.ts
+++ src/gameserver.ts
@@ -6,5 +6,6 @@ import type { pbMessage } from "@thegraid/wspbclient";
 class GameServer<T extends pbMessage> extends EchoDriver<T> {
 
 }
-
+// TODO: define GammaCgServer extends CgServerDriver<GammaProto>
+// implement logic to setup Map, send Reveal or Model messages to gammaNg client.
 wssServer(true, 'cgserver', srvrOpts('game7', '8446'), CgServerDriver)
